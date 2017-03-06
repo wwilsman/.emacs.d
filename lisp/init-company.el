905344196b0f607760b40ce50738a0ea9a727fb8
Wil Wilsman <wilw@spiceworks.com>
2017-03-06 14:31:55 -0600

    Fix lowercase completions

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,7 +1,8 @@
 (use-package company
   :diminish company-mode
   :config
-  (global-company-mode +1))
+  (global-company-mode +1)
+  (setq company-dabbrev-downcase nil))
 
 ;; provide this module
 (provide 'init-company)
