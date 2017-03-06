36340c10b8a06d87036f9c470a7f2d1f58cac298
Wil Wilsman <wilw@spiceworks.com>
2017-03-06 14:33:37 -0600

    Add other useful crux commands

--- lisp/init-crux.el
+++ lisp/init-crux.el
@@ -3,7 +3,10 @@
          ([(control shift return)] . crux-smart-open-line-above)
          ([remap move-beginning-of-line] . crux-move-beginning-of-line)
          ([remap kill-whole-line] . crux-kill-whole-line)
-         ("C-<backspace>" . crux-kill-line-backwards)))
+         ("C-<backspace>" . crux-kill-line-backwards)
+         ("C-c d" . crux-duplicate-current-line-or-region)
+         ("C-c D" . crux-delete-file-and-buffer)
+         ("C-c r" . crux-rename-file-and-buffer)))
 
 ;; provide this module
 (provide 'init-crux)
