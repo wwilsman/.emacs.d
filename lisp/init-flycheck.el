cc09c0b8bd5611f995ed725482c7d585833a8f90
Wil Wilsman <wilw@spiceworks.com>
2017-03-24 15:52:32 -0500

    Use "!" as flycheck indicator

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -3,8 +3,39 @@
   :config
   (setq flycheck-indication-mode 'right-fringe)
 
-  (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
-    [0 0 0 0 0 4 12 28 60 124 252 124 60 28 12 4 0 0 0 0])
+  (define-fringe-bitmap 'ww/flycheck-fringe-indicator
+    (vector #b00000000
+            #b00011000
+            #b00011000
+            #b00011000
+            #b00011000
+            #b00011000
+            #b00011000
+            #b00000000
+            #b00000000
+            #b00011000
+            #b00011000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000))
+
+  (flycheck-define-error-level 'error
+    :severity 2
+    :overlay-category 'flycheck-error-overlay
+    :fringe-bitmap `ww/flycheck-fringe-indicator
+    :fringe-face 'flycheck-fringe-error)
+  (flycheck-define-error-level 'warning
+    :severity 1
+    :overlay-category 'flycheck-warning-overlay
+    :fringe-bitmap `ww/flycheck-fringe-indicator
+    :fringe-face 'flycheck-fringe-warning)
+  (flycheck-define-error-level 'info
+    :severity 0
+    :overlay-category 'flycheck-info-overlay
+    :fringe-bitmap `ww/flycheck-fringe-indicator
+    :fringe-face 'flycheck-fringe-info)
 
   (flycheck-add-mode 'javascript-eslint 'web-mode)
   (add-hook 'prog-mode-hook 'flycheck-mode))
