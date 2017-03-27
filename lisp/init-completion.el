430b6a255fac54d59db72098e4b1ed0079cbfa37
Wil Wilsman <wilw@spiceworks.com>
2017-03-27 12:26:43 -0500

    Bind ivy-alt-done to `RET` in counsel-find-file
    This prevents opening directories in dired and instead descends into that
    directory similar to IDO

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -4,12 +4,13 @@
 ;; completion frontend & search
 (use-package swiper
   :diminish ivy-mode
-  :bind (
-         ([remap find-file] . counsel-find-file)
+  :bind (([remap find-file] . counsel-find-file)
          ([remap describe-function] . counsel-describe-function)
          ([remap describe-variable] . counsel-describe-variable)
          ([remap find-library] . counsel-find-library)
-         ([remap execute-extended-command] . counsel-M-x))
+         ([remap execute-extended-command] . counsel-M-x)
+         :map counsel-find-file-map
+         ([remap ivy-done] . ivy-alt-done))
   :init
   (setq ivy-use-virtual-buffers t)
   :config
