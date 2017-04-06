5e770be70af812710f5362e4505e81169752cee4
Wil Wilsman <wil.wilsman@gmail.com>
2017-04-06 18:47:03 -0500

    Set sgml indentation offsets

--- lisp/lang-javascript.el
+++ lisp/lang-javascript.el
@@ -1,6 +1,8 @@
 ;; js2/jsx mode
 (use-package js2-mode
   :init
+  (setq sgml-basic-offset 2)
+  (setq sgml-attribute-offset 2)
   (setq js2-basic-offset 2)
   (setq js2-strict-missing-semi-warning nil)
   (setq js2-missing-semi-one-line-override t)
