dcf6810bb7fa41c3d8632f01a44946a19161204f
Wil Wilsman <hello@wilwilsman.com>
2019-02-19 19:11:23 -0600

    Cleanup line numbering

--- lisp/init-linum.el
+++ lisp/init-linum.el
@@ -2,16 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 (require 'use-package)
+(require 'linum)
 
-(global-linum-mode t)
+(defun ww/linum-format (line)
+  "Format LINE numbers to three digits with a trailing and leading space."
+  (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+    (propertize (format (format " %%%dd " w) line) 'face 'linum)))
+(setq linum-format 'ww/linum-format)
+(add-hook 'prog-mode-hook 'linum-mode)
 
 ;; highlight line numbers
 (use-package hlinum
-  :custom
-  (linum-format
-   (lambda (line)
-     (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
-       (propertize (format (format "%%%dd " w) line) 'face 'linum))))
   :config
   (hlinum-activate))
 
