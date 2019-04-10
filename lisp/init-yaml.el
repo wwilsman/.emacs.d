44b53f1355bbfdb01a58426293dc9f26d7f2edc4
Wil Wilsman <wil@wilwilsman.com>
2019-04-10 12:47:49 -0500

    Add yaml-mode

---
+++ lisp/init-yaml.el
@@ -0,0 +1,10 @@
+;;; init-yaml.el --- YAML mode
+;;; Commentary:
+;;; Code:
+(require 'use-package)
+
+(use-package yaml-mode
+  :mode ("\\.yaml\\'"))
+
+(provide 'init-yaml)
+;;; init-yaml.el ends here
