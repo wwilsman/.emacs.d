483d3b83d43e91907f693af9a7c7427836884cd8
Wil Wilsman <wil@wilwilsman.com>
2019-04-15 15:39:36 -0500

    Have projectile-run-term open named multi-term buffer
    C-p x t

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -6,7 +6,18 @@
 (use-package multi-term
   :custom
   (multi-term-program "/bin/zsh")
-  (term-suppress-hard-newline t))
+  (term-suppress-hard-newline t)
+  :config
+  (defun projectile-run-term ()
+    "Get dedicated multi-term in project root"
+    (interactive)
+    (setq projectile--proj-term-name
+          (concat "*" multi-term-buffer-name "<" (projectile-project-name) ">*"))
+    (if (not (eq nil (get-buffer projectile--proj-term-name)))
+        (switch-to-buffer projectile--proj-term-name)
+      (projectile-with-default-dir (projectile-project-root)
+        (multi-term)
+        (rename-buffer projectile--proj-term-name)))))
 
 (provide 'init-term)
 ;;; init-term.el ends here
