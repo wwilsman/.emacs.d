abb76ae83a16106b46a72e2f235833cf2e2fcb14
Wil Wilsman <wil@wilwilsman.com>
2020-04-25 22:50:56 -0500

    🚸 Use a default spaceline modeline with no window-system

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -10,6 +10,15 @@
 
 (use-package spaceline
   :init
+  (unless window-system (spaceline-emacs-theme)))
+
+(use-package spaceline-all-the-icons
+  :custom
+  (spaceline-all-the-icons-separator-type 'arrow)
+  :init
+  ;; spaceline-all-the-icons does not come with diff-hl support out of the box so
+  ;; we redefine it's git-stat function to gather diff info from diff-hl instead
+  ;; of git-gutter.
   (defun ww/diff-hl-reducer (acc it)
     "A reducer to count added, removed, and modified lines from diff-hl.
 Inserts, deletes, or changes lines in ACC according to IT."
@@ -20,23 +29,16 @@ Inserts, deletes, or changes lines in ACC according to IT."
           ('insert (list (+ added lines) removed modified))
           ('delete (list added (+ removed lines) modified))
           ('change (list added removed (+ modified lines)))))))
-  :config
-  ;; spaceline-all-the-icons does not come with diff-hl support out of the box so
-  ;; we redefine it's git-stat function to gather diff info from diff-hl instead
-  ;; of git-gutter.
+
   (defun spaceline-all-the-icons--git-statistics ()
     "Return a list of added, removed, and modified lines in the current file.
 Uses diff-hl as oppose to the default git-gutter functions."
     (cond
      ((bound-and-true-p diff-hl-mode)
       (cl-reduce 'ww/diff-hl-reducer (diff-hl-changes) :initial-value '(0 0 0)))
-     (t '(0 0 0)))))
+     (t '(0 0 0))))
 
-(use-package spaceline-all-the-icons
-  :custom
-  (spaceline-all-the-icons-separator-type 'arrow)
-  :init
-  (spaceline-all-the-icons-theme))
+  (if window-system (spaceline-all-the-icons-theme)))
 
 (provide 'init-modeline)
 ;;; init-modeline.el ends here
