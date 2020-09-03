ca14c6c11706304224a43e62ad7da0d3feac1a96
Wil Wilsman <wil@wilwilsman.com>
2020-09-02 19:57:30 -0500

    ✨ Clean up stale buffers every 2 hours

--- lisp/init-session.el
+++ lisp/init-session.el
@@ -3,21 +3,39 @@
 ;;; Code:
 (require 'desktop)
 (require 'savehist)
+(require 'midnight)
 
 ;; persist buffers
-(setq desktop-dirname (expand-file-name "desktop/" user-emacs-directory)
-      desktop-base-file-name "emacs.desktop"
-      desktop-base-lock-name "lock"
-      desktop-path (list desktop-dirname)
-      desktop-save t
-      desktop-restore-frames t
-      desktop-load-locked-desktop t
-      desktop-auto-save-timeout 30)
+(setq
+ desktop-dirname (expand-file-name "desktop/" user-emacs-directory)
+ desktop-base-file-name "emacs.desktop"
+ desktop-base-lock-name "lock"
+ desktop-path (list desktop-dirname)
+ desktop-save t
+ desktop-restore-frames t
+ desktop-load-locked-desktop t
+ desktop-auto-save-timeout 30)
 (desktop-save-mode 1)
 
 ;; persist kill-ring
 (savehist-mode 1)
 (add-to-list 'savehist-additional-variables 'kill-ring)
+
+;; clean up untouched buffers
+(defvar clean-buffer-list-timer nil
+  "Stores 'clean-buffer-list' timer if there is one.
+Disable 'clean-buffer-list' with (cancel-timer clean-buffer-list-timer).")
+
+(setq
+ ;; kill buffers if they were last touched more than 15 minutes ago
+ clean-buffer-list-delay-special 900
+ ;; run clean-buffer-list every 2 hours
+ clean-buffer-list-timer (run-at-time t 7200 'clean-buffer-list)
+ ;; kill everything, clean-buffer-list is very intelligent at not killing unsaved buffers
+ clean-buffer-list-kill-regexps '("^.*$")
+ ;; always keep these buffer alive
+ clean-buffer-list-kill-never-buffer-names
+ (append '("*Messages*" "*cmd*" "*scratch*") clean-buffer-list-kill-never-buffer-names))
 
 (provide 'init-session)
 ;;; init-session.el ends here
