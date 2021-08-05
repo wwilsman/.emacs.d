ccf52906dcbd1ed0f7569055ffde56bfa8fbed6c
Wil Wilsman <wil@wilwilsman.com>
2021-08-04 19:40:17 -0500

    ✨ Update font to Operator Mono Lig

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -1,65 +1,69 @@
 ;;; init-font.el --- Font settings
 ;;; Commentary:
 ;;; Code:
-(setq-default line-spacing 0.5)
+(setq-default line-spacing 0.4)
 
-;; set up "Fira Code" font + ligatures
-(when (find-font (font-spec :name "Fira Code"))
-  (add-to-list 'default-frame-alist '(font . "Fira Code-12"))
-  (set-face-attribute 'default t :font "Fira Code-12")
+;; set up font + ligatures
+(defun ww/init-font ()
+  "Initialize font-face attributes."
+  (when (find-font (font-spec :name "Operator Mono Lig"))
+    (add-to-list 'default-frame-alist '(font . "Operator Mono Lig"))
+    (set-face-attribute 'default nil :font "Operator Mono Lig" :height 120)
 
-  ;; "Fira Code" ligatures
-  (dolist (char-regexp
-           '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
-             (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
-             (36 . ".\\(?:>\\)")
-             (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
-             (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
-             (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
-             (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
-             (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
-             (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
-             (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
-             (48 . ".\\(?:x[a-zA-Z]\\)")
-             (58 . ".\\(?:::\\|[:=]\\)")
-             (59 . ".\\(?:;;\\|;\\)")
-             (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
-             (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
-             (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
-             (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
-             (91 . ".\\(?:]\\)")
-             (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
-             (94 . ".\\(?:=\\)")
-             (119 . ".\\(?:ww\\)")
-             (123 . ".\\(?:-\\)")
-             (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
-             (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")))
-    (set-char-table-range composition-function-table (car char-regexp)
-                          `([,(cdr char-regexp) 0 font-shape-gstring])))
+    ;; ligatures
+    (dolist (char-regexp
+             '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+               (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
+               (36 . ".\\(?:>\\)")
+               (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
+               (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+               (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
+               (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
+               (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+               (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+               (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+               (48 . ".\\(?:x[a-zA-Z]\\)")
+               (58 . ".\\(?:::\\|[:=]\\)")
+               (59 . ".\\(?:;;\\|;\\)")
+               (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
+               (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+               (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+               (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+               (91 . ".\\(?:]\\)")
+               (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+               (94 . ".\\(?:=\\)")
+               (119 . ".\\(?:ww\\)")
+               (123 . ".\\(?:-\\)")
+               (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+               (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")))
+      (set-char-table-range composition-function-table (car char-regexp)
+                            `([,(cdr char-regexp) 0 font-shape-gstring])))
 
-  ;; use "Operator Mono" for cursive keywords
-  (when (find-font (font-spec :name "Operator Mono"))
-    (set-face-attribute 'font-lock-builtin-face nil :family "Operator Mono" :italic t)
-    (set-face-attribute 'font-lock-constant-face nil :family "Operator Mono" :italic t)
-    (set-face-attribute 'font-lock-keyword-face nil :family "Operator Mono" :italic t)
-    (set-face-attribute 'font-lock-type-face nil :family "Operator Mono" :italic t)
-    (set-face-attribute 'font-lock-function-name-face nil :family "Operator Mono" :italic t)))
+    ;; cursive keywords
+    (set-face-italic 'font-lock-builtin-face t)
+    (set-face-italic 'font-lock-constant-face t)
+    (set-face-italic 'font-lock-keyword-face t)
+    (set-face-italic 'font-lock-type-face t)
+    (set-face-italic 'font-lock-function-name-face t)
 
-;; set web-mode specific face-attributes
-(defun ww/web-mode-face-attributes ()
-  "Set font face attributes for web-mode."
-  (when (find-font (font-spec :name "Operator Mono"))
-    (set-face-attribute 'web-mode-html-attr-name-face nil :family "Operator Mono" :italic t)
-    (set-face-attribute 'web-mode-css-property-name-face nil :family "Operator Mono" :italic t)
-    (set-face-attribute 'web-mode-css-pseudo-class-face  nil :family "Operator Mono" :italic t)))
-(add-hook 'web-mode-hook 'ww/web-mode-face-attributes)
+    ;; set web-mode specific face-attributes
+    (defun ww/web-mode-face-attributes ()
+      "Set font face attributes for web-mode."
+      (set-face-italic 'web-mode-html-attr-name-face t)
+      (set-face-italic 'web-mode-css-property-name-face t)
+      (set-face-italic 'web-mode-css-pseudo-class-face t))
+    (add-hook 'web-mode-hook 'ww/web-mode-face-attributes)
 
-;; set rjsx-mode specific face-attributes
-(defun ww/rjsx-mode-face-attributes ()
-  "Set font face attributes for rjsx-mode."
-  (when (find-font (font-spec :name "Operator Mono"))
-    (set-face-attribute 'rjsx-attr nil :family "Operator Mono" :italic t)))
-(add-hook 'rjsx-mode-hook 'ww/rjsx-mode-face-attributes)
+    ;; set rjsx-mode specific face-attributes
+    (defun ww/rjsx-mode-face-attributes ()
+      "Set font face attributes for rjsx-mode."
+      (set-face-italic 'rjsx-attr t))
+    (add-hook 'rjsx-mode-hook 'ww/rjsx-mode-face-attributes)))
+
+;; in deamons, register font init after frame creation
+(if (not (daemonp)) (ww/init-font)
+  (add-hook 'after-make-frame-functions
+            (lambda (frame) (select-frame frame) (ww/init-font))))
 
 (provide 'init-font)
 ;;; init-font.el ends here
