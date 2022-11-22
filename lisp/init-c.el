0ce95efd617ad25ca42fcb97cab4d1edbea6402a
Wil Wilsman <wil@wilwilsman.com>
2022-11-22 11:17:35 -0600

    ✨ Add csharp-mode

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -1,9 +1,13 @@
 ;;; init-c.el --- C, C#, C++ modes
 ;;; Commentary:
 ;;; Code:
+(require 'use-package)
 
 (setq-default c-basic-offset 4)
 (add-to-list 'auto-mode-alist '("\\.(ino|c)\\'" . c-mode))
+
+(use-package csharp-mode
+  :mode "\\.cs\\'")
 
 (provide 'init-c)
 ;;; init-c.el ends here
