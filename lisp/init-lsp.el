b9500dcbfa09069626104b89d74c5c035e7d7f39
Wil Wilsman <wil@wilsman.dev>
2023-04-05 12:17:20 -0500

    ♻ Move lsp-mode settings into config block
    Using setq will trigger any defined setters while using custom will not

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -8,21 +8,25 @@
   :hook ((web-mode . lsp-deferred)
          (lsp-mode . lsp-enable-which-key-integration))
   :custom
-  (lsp-keymap-prefix "C-c l")
-  (lsp-eldoc-enable-hover nil)
-  (lsp-signature-auto-activate nil)
-  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-eslint-package-manager "yarn")
   :config
   (setf (alist-get 'web-mode lsp--formatting-indent-alist) 'web-mode-code-indent-offset)
-  (add-to-list 'lsp-language-id-configuration '("\\.hbs?\\'" . "html")))
+  (add-to-list 'lsp-language-id-configuration '("\\.hbs?\\'" . "html"))
+  (setq lsp-keymap-prefix "C-c l"
+        lsp-eldoc-enable-hover nil
+        lsp-signature-auto-activate nil
+        lsp-enable-on-type-formatting nil
+        lsp-headerline-breadcrumb-enable nil
+        ;lsp-eslint-server-command '("vscode-eslint-language-server" "--stdio")
+        lsp-eslint-validate '("javascript" "javascriptreact")))
 
 (use-package lsp-ui
   :commands lsp-ui-mode
-  :custom
-  (lsp-ui-sideline-show-code-actions t)
-  (lsp-ui-sideline-show-diagnostics t)
-  (lsp-ui-sideline-show-hover nil)
-  (lsp-ui-doc-enable nil))
+  :config
+  (setq lsp-ui-sideline-show-code-actions t
+        lsp-ui-sideline-show-diagnostics t
+        lsp-ui-sideline-show-hover nil
+        lsp-ui-doc-enable nil))
 
 (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
