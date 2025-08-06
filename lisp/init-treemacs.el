8b376b0d6f6161bada5135a0c3f5309efd408983
Wil Wilsman <wil@wilsman.dev>
2025-08-05 19:59:46 -0500

    🔧 Update treemacs config

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -3,6 +3,14 @@
 ;;; Code:
 (require 'use-package)
 
+(use-package tree-sitter
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :after tree-sitter)
+
 (use-package treemacs-all-the-icons
   :demand t
   :after (all-the-icons))
@@ -16,10 +24,11 @@
   (treemacs-project-follow-mode t)
   (treemacs-git-commit-diff-mode t)
   (treemacs-fringe-indicator-mode 'always)
-  (treemacs-load-all-the-icons-with-workaround-font "Courier New")
-  (treemacs-load-theme "all-the-icons")
+  ;;(treemacs-load-all-the-icons-with-workaround-font "Courier New")
+  ;;(treemacs-load-theme "all-the-icons")
 
   (setq treemacs-file-follow-delay 0
+        treemacs-no-png-images t
         treemacs--project-follow-delay 0
         treemacs-user-mode-line-format (list "")
         treemacs-user-header-line-format (list "")))
